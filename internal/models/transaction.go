49fd0ca2562009ad2afb46ce25704794f2b97367
Martin Dobrev <martindobrev87@gmail.com>
2025-02-11 13:40:41 +0200

    feat: entities models added

---
+++ internal/models/transaction.go
@@ -0,0 +1,15 @@
+package models
+
+type Transaction struct {
+	ID                int    `json:"id" gorm:"primaryKey"`
+	TransactionHash   string `json:"transactionHash" gorm:"unique;not null"`
+	TransactionStatus int    `json:"transactionStatus"`
+	BlockHash         string `json:"blockHash"`
+	BlockNumber       int    `json:"blockNumber"`
+	From              string `json:"from"`
+	To                string `json:"to"`
+	ContractAddress   string `json:"contractAddress"`
+	LogsCount         int    `json:"logsCount"`
+	Input             string `json:"input"`
+	Value             int    `json:"value"`
+}
